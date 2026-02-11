74db9c990c34e6721eb590f23b485741b4f8de85
AlessandroMessineo <alessandromessineo46@gmail.com>
2026-02-11 17:53:23 +0100

    implementata la show e creata pagina detail pizza

--- src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
+++ src/main/java/org/lessons/java/spring_la_mia_pizzeria_crud/controller/PizzaController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.lessons.java.spring_la_mia_pizzeria_crud.classes.Pizza;
 
@@ -27,4 +28,12 @@ public class PizzaController {
 
         return "pizzas/index";
     }
+
+    @GetMapping("/{id}")
+    public String show(Model model, @PathVariable Integer id){
+
+        model.addAttribute("pizza", repository.findById(id).get());
+        return "pizzas/detail-pizza";
+    }
+
 }
